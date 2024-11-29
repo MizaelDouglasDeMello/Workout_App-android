42acf9c6763c09efbb92b65e0a1abd480f330132
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-11-29 13:16:39 -0300

    feat: intro Screen Finalized

--- app/src/main/java/br/com/mizaeldouglas/workout_app/IntroActivity.kt
+++ app/src/main/java/br/com/mizaeldouglas/workout_app/IntroActivity.kt
@@ -1,20 +1,31 @@
 package br.com.mizaeldouglas.workout_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.mizaeldouglas.workout_app.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityIntroBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        binding.startBtn.setOnClickListener{
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
